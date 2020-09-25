1ad80e62eacecc0e9862975dec18c011ff81088c
Divu73 <divyanshg.p20076@iimtrichy.ac.in>
2020-09-25 22:50:54 +0530

    0

--- DecisionTree Exercise.py
+++ DecisionTree Exercise.py
@@ -29,3 +29,26 @@ x_test.shape
 from sklearn.tree import DecisionTreeClassifier
 Model=DecisionTreeClassifier()
 Model.fit(x_train,y_train)
+
+ypred1 = Model.predict(x_test)
+ypred1
+
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+classification_report(y_true=y_test, y_pred= ypred1)
+confusion_matrix(y_true=y_test, y_pred=ypred1)
+accuracy_score(y_true=y_test, y_pred=ypred1)
+
+
+newdata=x.sample(7)
+newdata
+ypred2 = Model.predict(newdata)
+ypred2
+classification_report(y_true=y_test, y_pred= ypred2)
+confusion_matrix(y_true=y_test, y_pred=ypred1)
+y_test
+
+#pip install graphviz
+from graphviz import Source
+from sklearn import tree
+tree.plot_tree(decision_tree=Model)
+
